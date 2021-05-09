d36f4a578ca73b18e0be88ca1f21b22d1ed6d0fd
marianaf <mlfernandez@hotmail.com>
2021-05-09 23:14:35 +0200

    comenzando a declarar y maquetar el js

---
+++ js/mainTest.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userGrid = document.querySelector(".gridUser")
+    const computerGrid = document.querySelector(".gridComputer")
+    const displayGrid = document.querySelector(".gridDisplay")
+    const ship = document.querySelectorAll(".ship")
+    const destroyed = document.querySelector(".destroyedContainer")
+    const submarine = document.querySelector(".sumarineContainer")
+    const crusier = document.querySelector(".crusierContainer")
+    const battleship = document.querySelector(".battleshipContainer")
+    const carrier = document.querySelector(".carrierContainer")
+    const startButton = document.querySelector('#start')
+    const rotateButton = document.querySelector('#rotate')
+    const turnDisplay = document.querySelector('#whoseGo')
+    const infoDisplay = document.querySelector('#info')
+    const userSquares = []
+    const computerSquares = []
+
+
+    // Create Board
+
+    function createBoard(grid, squares) {
+        for (let i = 0; i < width*width; i++) {
+            const square = document.createElement('div')
+            square.dataset.id = 1
+            grid.appendChild(square) //antes le puso userGrid pero al pasarle el parametro grid lo cambio, para hacer la funcion de abajo createBoard
+            squares.push(square) // idem, era userSquares
+        }
+    }
+    createBoard(userGrid, userSquares)
+    createBoard(computerGrid, computerSquares)
+
+    // Ships
+
+    const shipArray = [ 
+        { 
+            name: 'destroyed',
+            directions: [
+                [0, 1],
+                [0, width],
+            ]
+        },
+        {
+            name: 'submarine',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2],
+            ]
+        },
+        {
+            name: 'crusier',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2],
+            ]
+        },
+        {
+            name: 'battleship',
+            directions: [
+                [0, 1, 2, 3],
+                [0, width, width*2, width*3],
+            ]
+        },
+        {
+            name: 'carrier',
+            directions: [
+                [0, 1, 2, 3, 4],
+                [0, width, width*2, width*3, width*4],
+            ]
+        },
+    ]
+
+    // Draw the computer ships in random locations
+    function generate(ship) {
+        let randomDirection = Math.floor(Math.random() * ship.directions.length)
+        let current = ship.directions[randomDirection]
+        if (randomDirection === 0) direction = 1
+        if (randomDirection === 1) direction = 10
+        let randomStart = Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * directions))
+
+        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
+        const isAtRightEdge = current.some(idex => (randomStart + index) % width === width - 1) // el modulo da lo que sig q esta del lado right
+        const isAtLeftEdge = current.some(idex => (randomStart + index) % width === 0) // si es del left va a dar 0 el barco esta en el borde
+
+        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ships.name)
+    }
+
+
+
+})
