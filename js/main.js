5937f3f31576bab5284f8d1ab6cbf93bbeec11e1
marianaf <mlfernandez@hotmail.com>
2021-05-10 23:15:12 +0200

    creando clases y maquetado de pantallas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-//ESCONDER PANTALLA 0, APARACE PANTALLA 1
-
-//const cambiarPantalla = (pantalla) => {
+//ESCONDER PANTALLA
 
 let pantalla = "screen0";
 
-document.addEventListener('keypress', function (event) {
-
+document.addEventListener("keypress", function (event) {
   //alert(pantalla);
   // cuando entra en pantalla 1
   if (pantalla === "screen0") {
@@ -15,14 +12,14 @@ document.addEventListener('keypress', function (event) {
     document.getElementById("idPantalla3").style.display = "none";
     pantalla = "screen1";
 
- // cuando entra en pantalla 2
+    // cuando entra en pantalla 2
   } else if (pantalla === "screen1") {
     document.getElementById("idPantalla0").style.display = "none";
     document.getElementById("idPantalla1").style.display = "none";
     document.getElementById("idPantalla2").style.display = "inline";
     document.getElementById("idPantalla3").style.display = "none";
     pantalla = "screen2";
- 
+
     // cuando entra en pantalla 3
   } else if (pantalla === "screen2") {
     document.getElementById("idPantalla0").style.display = "none";
@@ -30,27 +27,57 @@ document.addEventListener('keypress', function (event) {
     document.getElementById("idPantalla2").style.display = "none";
     document.getElementById("idPantalla3").style.display = "inline";
     pantalla = "screen3";
-
   } else {
     //alert("falta hacer mas pantallas");
-  
   }
-})
-//}
+});
 
-/*
-const hiddenFunction = (pantalla) => {
-  let arrScreen= ["idPantalla0", "idPantalla1", "idPantalla2", "idPantalla3"];
+// DECLARANDO CLASES
 
-  arrScreen = arrScreen.filter(val => !pantalla.includes(val));
-
-  document.getElementById(pantalla).style.display = "block";
-
-  for (let_screen of arrScreen) {
-      document.getElementById(_screen).style.display = "none";
+class Tablero {
+  constructor(zona) {
+    this.zona = zona;
+    this.size = 9;
   }
 
+  dibujar() {
+    //mostrar el tablero
+  }
 
+  asignar() {
+    // asignar ubicacion zona
+  }
+
+  mostrarHundido() { }
+
+  mostrarAgua() { }
+
+  mostrarFuego() { }
 }
 
-*/
+class Barco {
+  constructor(zona) {
+    this.zona = zona;
+  }
+
+  disparar(aquebarco, aquezona) { 
+//TODO el barco dispara al barco enemigo en una zona
+//barcoEnemigo.observar(zona)
+
+  }
+
+  observar() { 
+//TODO se recibe un ataque se verifica si el ataque es de la misma 
+// zona donde esta el barco, si es devuelve hundido, en caso contrario
+// devuelve agua
+
+
+  }
+}
+
+let barco1 = new Barco("A");
+//alert(barco1.ubicacion);
+
+let barco2 = new Barco("F");
+//alert(barco2.ubicacion);
+
