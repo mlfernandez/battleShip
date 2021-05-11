634c229ca29f2f1b0e4756bf8e4c539fa7a5f97d
marianaf <mlfernandez@hotmail.com>
2021-05-11 13:51:13 +0200

    trabajando en la seleccion de barcos

--- js/main.js
+++ js/main.js
@@ -114,3 +114,44 @@ const chooseShip = (ship) => {
   }
     
 */
+
+class Barco{
+
+  constructor(nombre,size){
+      this.vida = 5;
+      this.nombre = nombre;
+      this.zona = Math.floor(Math.random() *10);
+      this.size = size
+      /*this.suerte = Math.floor(Math.random() *11);
+      this.ataque = ataque;
+      this.defensa = defensa;
+      this.tecnica = tecnica;
+      this.velocidad = velocidad;*/
+
+  }
+
+
+  hit(enemy){
+      enemy.vida -= this.ataque - (enemy.defensa * (enemy.suerte / this.velocidad));
+  }
+
+  
+  specialHit(enemy){
+      enemy.vida -= this.ataque + this.tecnica - (enemy.defensa * (enemy.suerte / this.velocidad));
+  }
+}
+
+
+//Instancio luchadores
+let player1 = new Barco("Aircraft",1);
+let player2 = new Barco("Carrier",2);
+let player3 = new Barco("Submarine",3);
+let player4 = new Barco("Warship",4);
+
+
+let allPlayers = {
+  "1" : player1,
+  "2" : player2,
+  "3" : player3,
+  "4" : player4
+}
