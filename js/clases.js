3eb288ec175b74c474038d06772dd5979332fd94
marianaf <mlfernandez@hotmail.com>
2021-05-13 23:32:05 +0200

    trabajando en resolucion de logica de juego, contador de municiones, empates

---
+++ js/clases.js
@@ -0,0 +1,155 @@
+class Barco {
+  constructor(game, nombre, municiones) {
+    this.game = game;
+    this.nombre = nombre;
+    this.zonaUbicacion = Math.floor(Math.random() * 8) + 1;
+    this.municiones = municiones;
+  }
+
+  disparar(aQueBarco, aQueZona) {
+    // el barco dispara al barco enemigo en una zona
+    //barcoEnemigo.observar(zona)
+
+    // se comprueba que tenga municiones para poder disparar
+    if (this.municiones === 0) {
+    } else {
+      // tiene municiones, descontamos 1 municion
+      this.municiones = this.municiones - 1;
+
+      // disparamos
+      setTimeout(() => aQueBarco.observar(aQueZona), 2000);
+    }
+  }
+
+  observar(zonaAtacada) {
+    // se recibe un ataque se verifica si el ataque es de la misma
+    // zona donde esta el barco, si es invoca a hundido, en caso contrario
+    // devuelve agua
+    alert(
+      "esta observando, barco ubicado en" +
+        this.zonaUbicacion +
+        " zona atacada" +
+        zonaAtacada
+    );
+    if (this.zonaUbicacion == zonaAtacada) {
+      this.game.mostrarHundido(zonaAtacada);
+      this.game.mostrarGanador(turnoJugador);
+    } else {
+      //alert("va a mostrar agua");
+      this.game.mostrarAgua(zonaAtacada);
+    }
+  }
+}
+
+class Game {
+  constructor() {
+    //this.zona = zona;
+    //this.sizeTablero = 9;
+    this.turnoJugador = 1;
+  }
+
+  mostrarTurno() {
+    //alert("Es el turno del jugador " + this.turnoJugador + " ya puede disparar a una zona del tablero.");
+    document.getElementById("turno").innerText =
+      "Es el turno del jugador " +
+      this.turnoJugador +
+      ", ya puede disparar a una zona del tablero.";
+  }
+
+  mostrarHundido(zona) {
+    alert("Hundido!!!");
+    //poner imagen de hundido
+    this.crearAnimacion("img/sunken3.png", "agua", "zona" + zona);
+
+    this.reproducirSonido("audio/hundidoS.mp3");
+
+    setTimeout(() => cambiarPantalla("idPantalla5"), 2000);
+
+    document.getElementById("winner").innerText =
+      "El ganador es el jugador " +
+      this.turnoJugador +
+      ", presiona cualquier tecla para volver a jugar.";
+
+    //TODO mostrar fin de juego y opcion restart
+  }
+
+  mostrarAgua(zona) {
+    // alert("Agua!!!" + this.turnoJugador);
+    // pasar el turno al siguiente jugador y mostrar turno
+
+    if (this.turnoJugador == 1) {
+      this.turnoJugador = 2;
+    } else {
+      this.turnoJugador = 1;
+    }
+
+    //mostrar imagen de agua en zona y sonido
+
+    this.crearAnimacion("img/water.png", "agua", "zona" + zona);
+    this.reproducirSonido("audio/aguaS.mp3");
+
+    this.mostrarTurno();
+  }
+
+  crearAnimacion(archivoImagen, claseImagen, idzona) {
+    //crea elemento imagen
+    let img = document.createElement("img");
+
+    // define las propiedades
+    img.src = archivoImagen; //"img/water.png";
+    img.className = claseImagen; //"agua";
+
+    //busca donde insertar el elemento
+    let src = document.getElementById(idzona); //"zona" + zona);
+
+    //agrega el nuevo elemento
+    src.appendChild(img);
+
+    // espera 1 segundo y elimina el elemento recien creado
+    setTimeout(() => src.removeChild(img), 2000);
+
+    // fin seccion mostrar agua
+  }
+
+  reproducirSonido(sonidoPath) {
+    let sonido = document.createElement("audio");
+    sonido.src = sonidoPath;
+    sonido.setAttribute("preload", "auto");
+    sonido.setAttribute("controls", "none");
+    sonido.style.display = "none";
+    document.body.appendChild(sonido);
+
+    sonido.play();
+  }
+
+  mostrarMuniciones(barcoJ1, barcoJ2) {
+    //TODO mostrar un cartel con la cantidad de municiones de cada barco
+    document.getElementById("municionesJ1").innerText =
+      "El jugador uno tiene " + barcoJ1.municiones + " municiones.";
+    document.getElementById("municionesJ2").innerText =
+      "El jugador dos tiene " + barcoJ2.municiones + " municiones.";
+  }
+  mostrarEmpate() {
+    alert('empate!');
+    cambiarPantalla('idPantalla5');
+  } 
+};
+
+const cambiarPantalla = (destinoPantalla) => {
+  let arrPantalla = [
+    "idPantalla0",
+    "idPantalla1",
+    "idPantalla2",
+    "idPantalla3",
+    "idPantalla4",
+    "idPantalla5",
+  ];
+
+  for (let i of arrPantalla) {
+    if (i == destinoPantalla) {
+      document.getElementById(i).style.display = "inline";
+    } else {
+      document.getElementById(i).style.display = "none";
+    }
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,39 +1,23 @@
 //ESCONDER PANTALLA
 
-let pantalla = "screen2";
-
 document.addEventListener("keypress", function (event) {
   //alert(pantalla);
   // cuando entra en pantalla 1
-  if (pantalla === "screen0") {
-    document.getElementById("idPantalla0").style.display = "none";
-    document.getElementById("idPantalla1").style.display = "inline";
-    document.getElementById("idPantalla2").style.display = "none";
-    document.getElementById("idPantalla3").style.display = "none";
-    document.getElementById("idPantalla4").style.display = "none";
-    document.getElementById("idPantalla5").style.display = "none";
-    pantalla = "screen1";
+  if (document.getElementById("idPantalla0").style.display === "inline") {
+    cambiarPantalla("idPantalla1");
 
     // cuando entra en pantalla 2
-  } else if (pantalla === "screen1") {
-    document.getElementById("idPantalla0").style.display = "none";
-    document.getElementById("idPantalla1").style.display = "none";
-    document.getElementById("idPantalla2").style.display = "inline";
-    document.getElementById("idPantalla3").style.display = "none";
-    document.getElementById("idPantalla4").style.display = "none";
-    document.getElementById("idPantalla5").style.display = "none";
-    pantalla = "screen2";
+  } else if (
+    document.getElementById("idPantalla1").style.display === "inline"
+  ) {
+    cambiarPantalla("idPantalla2");
 
     // cuando entra en pantalla 3 chooseShip
-  } else if (pantalla === "screen2") {
-    document.getElementById("idPantalla0").style.display = "none";
-    document.getElementById("idPantalla1").style.display = "none";
-    document.getElementById("idPantalla2").style.display = "none";
-    document.getElementById("idPantalla3").style.display = "inline";
-    document.getElementById("idPantalla4").style.display = "none";
-    document.getElementById("idPantalla5").style.display = "none";
-    pantalla = "screen3";
-  } else if (pantalla === "screen4") {
+  } else if (
+    document.getElementById("idPantalla2").style.display === "inline"
+  ) {
+    cambiarPantalla("idPantalla3");
+  } else if (document.getElementById("idPantalla4").style.display === "flex") {
     switch (event.key) {
       case "1":
       case "2":
@@ -45,170 +29,52 @@ document.addEventListener("keypress", function (event) {
       case "8":
       case "9":
         // alert(partida.turnoJugador);
+        partida.reproducirSonido("audio/canonS.mp3");
+        //setTimeout(() => (""), 2000);
+
         if (partida.turnoJugador == 1) {
           barcoJugador1.disparar(barcoJugador2, event.key);
-          
         } else {
           barcoJugador2.disparar(barcoJugador1, event.key);
         }
-        break;
-    }
+        // actualizo y muestro
+        partida.mostrarMuniciones(barcoJugador1, barcoJugador2);
+
+        //TODO chequear que queden municiones para poder disparar
+        if (barcoJugador1.municiones == 0) {
+          if (barcoJugador2.municiones == 0) {
+            partida.mostrarEmpate();
+          }
+        } 
+
+         break; 
+         
+        }
+        
+    
 
     // cuando entra en pantalla 5
-  } else if (pantalla === "screen4") {
-    document.getElementById("idPantalla0").style.display = "none";
-    document.getElementById("idPantalla1").style.display = "none";
-    document.getElementById("idPantalla2").style.display = "none";
-    document.getElementById("idPantalla3").style.display = "none";
-    document.getElementById("idPantalla4").style.display = "none";
-    document.getElementById("idPantalla5").style.display = "inline";
-    pantalla = "screen5";
-
-    // vuelve a la pantalla 3 (hay que cambiarlo)
+  } else if (
+    document.getElementById("idPantalla5").style.display === "inline"
+  ) {
+    //TODO reiniciar la partida
+    //1 - ir a pantalla 3 (elegir barco)
+    cambiarPantalla("idPantalla3");
   } else {
   }
 });
 
-class Barco {
-  constructor(nombre, resistencia) {
-    this.nombre = nombre;
-    this.zonaUbicacion = Math.floor(Math.random() * 8) + 1;
-    this.resistencia = resistencia;
-  }
-
-  disparar(Barco, aquezona) {
-    // el barco dispara al barco enemigo en una zona
-    //barcoEnemigo.observar(zona)
-
-
-    partida.reproducirSonido('audio/canonS.mp3');
-    setTimeout(() => Barco.observar(aquezona), 2000);
-    
-  }
-
-  observar(zonaAtacada) {
-    // se recibe un ataque se verifica si el ataque es de la misma
-    // zona donde esta el barco, si es invoca a hundido, en caso contrario
-    // devuelve agua
-    alert(
-      "esta observando, barco ubicado en" +
-        this.zonaUbicacion +
-        " zona atacada" +
-        zonaAtacada
-    );
-    if (this.zonaUbicacion == zonaAtacada) {
-      partida.mostrarHundido(zonaAtacada);
-    } else {
-      //alert("va a mostrar agua");
-      partida.mostrarAgua(zonaAtacada);
-    }
-  }
-}
-
-class Game {
-  constructor() {
-    //this.zona = zona;
-    //this.sizeTablero = 9;
-    this.turnoJugador = 1;
-  }
-
-  mostrarTurno() {
-    //alert("Es el turno del jugador " + this.turnoJugador + " ya puede disparar a una zona del tablero.");
-    document.getElementById("turno").innerText =
-      "Es el turno del jugador " +
-      this.turnoJugador +
-      ", ya puede disparar a una zona del tablero.";
-  }
-
-  mostrarHundido(zona) {
-    alert("Hundido!!!");
-    //poner imagen de hundido
-    this.crearAnimacion('img/sunken3.png', 'agua','zona'+ zona);
-    
-    this.reproducirSonido('audio/hundidoS.mp3');
-
-    //TODO mostrar fin de juego y opcion restart
-  }
-
-  mostrarAgua(zona) {
-    // alert("Agua!!!" + this.turnoJugador);
-    // pasar el turno al siguiente jugador y mostrar turno
-
-    if (this.turnoJugador == 1) {
-      this.turnoJugador = 2;
-    } else {
-      this.turnoJugador = 1;
-    }
-
-    //mostrar imagen de agua en zona
-    
-    this.crearAnimacion('img/water.png', 'agua','zona'+ zona);
-    this.reproducirSonido('audio/aguaS.mp3');
-
-    this.mostrarTurno();
-  }
-
-  mostrarFuego() {}
-
-  crearAnimacion(archivoImagen, claseImagen, idzona) {
-    
-    //crea elemento imagen
-    let img = document.createElement("img");
-    
-    // define las propiedades
-    img.src = archivoImagen; //"img/water.png";
-    img.className = claseImagen; //"agua";
-
-
-    //busca donde insertar el elemento
-    let src = document.getElementById(idzona); //"zona" + zona);
-
-    //agrega el nuevo elemento
-    src.appendChild(img);
-
-    // espera 1 segundo y elimina el elemento recien creado
-    setTimeout(() => src.removeChild(img), 2000);
-    
-
-    // fin seccion mostrar agua
-  }
-
-  reproducirSonido(sonidoPath) {
-    let sonido = document.createElement("audio");
-    sonido.src = sonidoPath;
-    sonido.setAttribute("preload", "auto");
-    sonido.setAttribute("controls", "none");
-    sonido.style.display = "none";
-    document.body.appendChild(sonido);
-    
-    sonido.play();
-
-  }
-}
-
-
-
-const chooseShip = (resistencia, tipoBarco) => {
-  //alert(resistencia + tipoBarco);
+const chooseShip = (municiones, tipoBarco) => {
+  //alert(game, municiones + tipoBarco);
   partida = new Game();
-  barcoJugador1 = new Barco(tipoBarco, resistencia);
-  barcoJugador2 = new Barco(tipoBarco, resistencia);
+  barcoJugador1 = new Barco(partida, tipoBarco, municiones);
+  barcoJugador2 = new Barco(partida, tipoBarco, municiones);
 
-  // voy a mostrar la pantalla tablero
-  document.getElementById("idPantalla0").style.display = "none";
-  document.getElementById("idPantalla1").style.display = "none";
-  document.getElementById("idPantalla2").style.display = "none";
-  document.getElementById("idPantalla3").style.display = "none";
+  // voy a mostrar la pantalla tablero screen4
+  cambiarPantalla("idPantalla4");
   document.getElementById("idPantalla4").style.display = "flex";
-  document.getElementById("idPantalla5").style.display = "none";
-  pantalla = "screen4";
   //poner en un div con el insert html
   partida.mostrarTurno();
 
-
-}
-
-
-
-
-
+  partida.mostrarMuniciones(barcoJugador1, barcoJugador2);
+};
